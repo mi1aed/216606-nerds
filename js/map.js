b8f1d3468e2548e65b4819c7177962f8c0b3e1cd
mi1aed <a.milaenko@gmail.com>
2016-09-03 17:09:25 +0700

    Мелкие фиксы по замечаниям + 2-я попытка подключить карту

--- js/map.js
+++ js/map.js
@@ -2,25 +2,24 @@ var container = document.querySelector("[id=yandex-map]");
 
 ymaps.ready(function () {
     var myMap = new ymaps.Map(container, {
-            center: [59.939239, 30.326853],
+            center: [59.939364, 30.329436],
             zoom: 16,
             controls: ["ZoomControl"]
         });
     myMap.behaviors.disable("scrollZoom");
     myMap.controls.add("zoomControl", {});
   
-    var myPlacemark = new ymaps.Placemark([59.939239, 30.326853] , {},
+    var myPlacemark = new ymaps.Placemark([59.938631, 30.323055] , {},
          {
             // Опции.
             // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
+            iconLayout: "default#image",
             // Своё изображение иконки метки.
-            iconImageHref: 'img/my-icon.png',
+            iconImageHref: "img/my-icon.png",
             // Размеры метки.
             iconImageSize: [231, 190],
             // Смещение левого верхнего угла иконки относительно
             // её "ножки" (точки привязки).
-            iconImageOffset: [-20, -142] });
 
     myMap.geoObjects.add(myPlacemark);
 });
