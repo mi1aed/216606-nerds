37256bf012b1289f77f99cf479f1bfc60928aa20
mi1aed <a.milaenko@gmail.com>
2016-09-03 09:05:28 +0700

    Сделал since 2004, разбираюсь с картой

--- js/icon_customImage.js
+++ js/icon_customImage.js
@@ -1,25 +1,26 @@
+var container = document.querySelector("[id=yandex-map]");
+
 ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [55.751574, 37.573856],
-            zoom: 9
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Собственный значок метки',
-            balloonContent: 'Это красивая метка'
-        }, {
+    var myMap = new ymaps.Map(container, {
+            center: [59.939239, 30.326853],
+            zoom: 16,
+            controls: ["ZoomControl"]
+        });
+    myMap.behaviors.disable("scrollZoom");
+    myMap.controls.add("zoomControl", {});
+  
+    var myPlacemark = new ymaps.Placemark([59.939239, 30.326853] , {},
+         {
             // Опции.
             // Необходимо указать данный тип макета.
             iconLayout: 'default#image',
             // Своё изображение иконки метки.
-            iconImageHref: '../img/my-icon.png',
+            iconImageHref: 'img/my-icon.png',
             // Размеры метки.
             iconImageSize: [231, 190],
             // Смещение левого верхнего угла иконки относительно
             // её "ножки" (точки привязки).
-            iconImageOffset: [-3, -42]
-        });
+            iconImageOffset: [-20, -142] });
 
     myMap.geoObjects.add(myPlacemark);
 });
