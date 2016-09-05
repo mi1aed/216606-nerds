c78c6bd9d0fda2c8517b7d469788107e88f1ce1c
mi1aed <a.milaenko@gmail.com>
2016-09-05 23:02:05 +0700

    Подготовил спрайт

--- js/map.js
+++ js/map.js
@@ -18,3 +18,23 @@ ymaps.ready(function ()  {
          });
     myMap.geoObjects.add(myPlacemark);
 });
+
+var feedbackOpen = document.querySelector(".btn-form");
+var feedbackPopup = document.querySelector(".modal-content");
+var feedbackClose = feedbackPopup.querySelector(".modal-content-close");
+
+feedbackOpen.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedbackPopup.classList.toggle("modal-content-show");
+});
+
+feedbackClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedbackPopup.classList.remove("modal-content-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27 && feedbackPopup.classList.contains("modal-content-show")) {
+    feedbackPopup.classList.remove("modal-content-show");
+  }
+});
